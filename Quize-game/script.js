2c5d8b3fd1b91bfe61616843ceb4a6f1c64b1526
AliAsjad512 <aa0504503@gmail.com>
2025-12-08 21:57:00 -0500

    score

--- Quize-game/script.js
+++ Quize-game/script.js
@@ -75,6 +75,7 @@ restartButton.addEventListener("click", restartQuiz);
 function startQuiz() {
     //console.log("Quiz started");
     currentQuestionIndex =0;
+    score =0;
     scoreSpan.textContent = 0;
     startScreen.classList.remove("active");
     quizScreen.classList.add("active");
